77b94fd6abf0cfeb192a991cf23847a8ee22178a
Ambika S <dsambika947@gmail.com>
2026-04-24 12:05:36 +0000

    macros and incremental runs

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,10 +14,7 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
-        --`amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
-        {# {{ cents_to_dollars('amount') }} as amount_usd #}
+        {{ cents_to_dollars('amount') }} as amount_usd
 
     from source
 
